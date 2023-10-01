5348a1738222adf507244ed33c3986c7219fd035
chandu sadda <chandusadda1992@gmail.com>
2023-10-01 18:01:49 +0530

    added header component

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const HeaderComp = () => {
-  return <div className="header-main">header</div>;
+  return <header className="header-main fixed top-0 right-0 left-auto px-6 flex flex-auto">
+    <div className="font-bold py-4">
+      Chandu Sadda
+    </div>
+    <div className="mr-5 ml-auto">
+      <a className="my-4 mx-6 ">Home</a>
+      <a className="py-4 mx-6 ">Education</a>
+      <a className="py-4 mx-6 ">Work</a>
+      <a className="py-4 mx-6 ">Contact</a>
+
+    </div>
+  </header>;
 };
 
 export default HeaderComp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-import ThemeToggle from '../components/ThemeToggle'
+import ThemeToggle from "../components/ThemeToggle";
+import HeaderComp from "./Header/Header";
 
 export default function Home() {
   return (
-    <div className="w-full container p-4 mx-auto">
-      <div className="py-20 flex flex-col items-center justify-center">
-        <h1 className="text-5xl text-center text-gray-800 dark:text-gray-100 font-bold">
-          Next Themes + Tailwind Dark Mode
-        </h1>
-        <p className="italic text-2xl">with app-dir</p>
+    <div>
+      <HeaderComp />
+      <div className="w-full container p-4 mx-auto">
+        <div className="py-20 flex flex-col items-center justify-center">
+          <h1 className="text-5xl text-center text-gray-800 dark:text-gray-100 font-bold">
+            Next Themes + Tailwind Dark Mode
+          </h1>
+          <p className="italic text-2xl">with app-dir</p>
 
-        <ThemeToggle />
+          <ThemeToggle />
+        </div>
       </div>
     </div>
-  )
+  );
 }
