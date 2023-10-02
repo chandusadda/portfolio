b5c4e82e910ea3aedb5f9720b8edc2f47ee97030
chandu sadda <chandusadda1992@gmail.com>
2023-10-02 12:47:47 +0530

    added few more styles header

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,18 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
-const HeaderComp = () => {
-  return <header className="header-main fixed top-0 right-0 left-auto px-6 flex flex-auto">
-    <div className="font-bold py-4">
-      Chandu Sadda
-    </div>
-    <div className="mr-5 ml-auto">
-      <a className="my-4 mx-6 ">Home</a>
-      <a className="py-4 mx-6 ">Education</a>
-      <a className="py-4 mx-6 ">Work</a>
-      <a className="py-4 mx-6 ">Contact</a>
+// NavBarElement component
+export function NavBarElement(props: any) {
+  return (
+    <Link href={`${props.pathname}`}>
+      <div>
+        <div className={`py-4 mx-8 mt-1 ${props.pathCheck ? "text-custom-purple" : "text-gray-500"}  font-medium hover:text-custom-purple`}>
+          {props.children}
+        </div>
+      </div>
+    </Link>
+  );
+}
 
-    </div>
-  </header>;
+const HeaderComp = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className="header-main fixed top-0 right-0 left-auto px-6 flex flex-auto">
+      <div className="font-bold py-4 text-2xl font-customFont">
+        Chandu <span className=" text-custom-purple">Sadda</span>
+      </div>
+      <div className="mr-5 ml-auto flex">
+        <NavBarElement
+          pathname="/"
+          pathCheck={
+            pathname === "/" || pathname === "/home/" || pathname === "/home"
+          }
+        >
+          Home
+        </NavBarElement>
+        <NavBarElement
+          pathname="/education"
+          pathCheck={pathname === "/education" || pathname === "/education/"}
+        >
+          Education
+        </NavBarElement>
+        <NavBarElement
+          pathname="/work"
+          pathCheck={pathname === "/work" || pathname === "/work/"}
+        >
+          Work
+        </NavBarElement>
+        <NavBarElement
+          pathname="/contact"
+          pathCheck={pathname === "/contact" || pathname === "/contact/"}
+        >
+          Contact
+        </NavBarElement>
+      </div>
+    </header>
+  );
 };
 
 export default HeaderComp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [],
-}
-export default config
+  theme: {
+    fontFamily: {
+      customFont: [
+        "Avenir Next",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+      ],
+    },
+    extend: {
+      colors: {
+        'custom-blue': '#1d9ded',
+        "custom-purple": "#3F0071"
+      },
+    }
+  },
+};
+export default config;
